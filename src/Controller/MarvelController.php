720d61898c5234a78d6a3904a86cc8ebf0a135a7
GrecoBen <greco.benjamin@orange.fr>
2023-08-21 15:28:39 +0200

    feat-api-marvel-connect

---
+++ src/Controller/MarvelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\MarvelApiUrlGenerator;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
+
+class MarvelController extends AbstractController
+{
+    /**
+     * @Route("/marvel", name="app_marvel")
+     */
+    public function index(): Response
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'MarvelController',
+        ]);
+    }
+
+    /**
+     * @Route("/get-marvel-info")
+     */
+    public function getMarvelInfo(MarvelApiUrlGenerator $urlGenerator): Response
+    {
+        // Specify the character name for which to retrieve information
+        $characterName = "Hulk";
+        
+        // Generate the URL for character information using the provided service
+        $url = $urlGenerator->generateCharacterUrl($characterName);
+
+        // Create an HTTP client instance
+        $httpClient = HttpClient::create();
+        
+        // Send a GET request to the generated URL
+        $response = $httpClient->request('GET', $url);
+
+        // Convert the JSON response to a PHP array
+        $data = $response->toArray();
+        
+        // Now you can process the received Marvel API data
+        // $data contains Marvel API information
+
+        // Create a response with the retrieved data
+        $responseContent = json_encode($data, JSON_PRETTY_PRINT);
+        return new Response($responseContent, 200, ['Content-Type' => 'application/json']);
+    }
+
+    /**
+     * @Route("/get-marvel-comics")
+     */
+    public function getMarvelComics(Request $request, MarvelApiUrlGenerator $urlGenerator): Response
+    {
+        // Retrieve the page number from the query parameters
+        $page = $request->query->getInt('page', 1);
+        
+        // Define the limit of items per page
+        $limit = 30;
+        
+        // Calculate the offset based on the page number
+        $offset = ($page - 1) * $limit;
+        
+        // Generate the URL for comics information using the provided service
+        $url = $urlGenerator->generateComicsUrl($limit, $offset);
+
+        // Create an HTTP client instance
+        $httpClient = HttpClient::create();
+        
+        // Send a GET request to the generated URL
+        $response = $httpClient->request('GET', $url);
+
+        // Convert the JSON response to a PHP array
+        $data = $response->toArray();
+
+        // Retrieve the comics data
+        $marvelComics = $data['data']['results'];
+
+        // Create a response with the retrieved data
+        $responseContent = json_encode($data, JSON_PRETTY_PRINT);
+        return new Response($responseContent, 200, ['Content-Type' => 'application/json']);
+    }
+}
---
+++ src/Service/MarvelApiUrlGenerator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+class MarvelApiUrlGenerator
+{
+    private $publicKey;
+    private $privateKey;
+
+    public function __construct(string $publicKey, string $privateKey)
+    {
+        // Store the provided public and private keys for authentication
+        $this->publicKey = $publicKey;
+        $this->privateKey = $privateKey;
+    }
+
+    public function generateCharacterUrl(string $characterName): string
+    {
+        // Create a timestamp for the current time
+        $ts = time();
+        
+        // Generate a hash using the timestamp, private key, and public key
+        $hash = md5($ts . $this->privateKey . $this->publicKey);
+
+        // Construct the character URL with the provided name, public key, timestamp, and hash
+        return sprintf(
+            'https://gateway.marvel.com:443/v1/public/characters?name=%s&apikey=%s&ts=%s&hash=%s',
+            $characterName,
+            $this->publicKey,
+            $ts,
+            $hash
+        );
+    }
+
+    public function generateComicsUrl($limit = 5, $offset = 0)
+    {
+        // Base URL for comics endpoint
+        $baseUrl = 'https://gateway.marvel.com:443/v1/public/comics';
+        
+        // Create a timestamp for the current time
+        $timestamp = time();
+        
+        // Generate a hash using the timestamp, private key, and public key
+        $hash = md5($timestamp . $this->privateKey . $this->publicKey);
+
+        // Construct the comics URL with timestamp, public key, hash, limit, and offset
+        $url = "$baseUrl?ts=$timestamp&apikey=$this->publicKey&hash=$hash&limit=$limit&offset=$offset";
+        
+        return $url;
+    }
+}
