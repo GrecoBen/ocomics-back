0355b84b8d69a6398c66b19d87ecadaa0d309707
NicolasDeniau <nicolas.deniau@oclock.school>
2023-08-22 14:33:37 +0200

    build-fixture-user_collection-done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,17 @@
 
 namespace App\DataFixtures;
 
-use App\DataFixtures\Provider\AppProvider;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Author;
 use App\Entity\Comics;
 use App\Entity\Characters;
-use App\Entity\User;
+use App\Entity\UserCollection;
 use Doctrine\Persistence\ObjectManager;
+use App\DataFixtures\Provider\AppProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -29,7 +30,7 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create("fr_FR");
         // ici j'ajoute mon provider personnalisé
-         $faker->addProvider(new AppProvider());
+        $faker->addProvider(new AppProvider());
 
         //! Author
         // Create an Author that will br link to a Comics
@@ -72,26 +73,46 @@ class AppFixtures extends Fixture
 
         //! USER
 
-        for ($i = 0; $i < 2; $i++) {
+        for ($l = 0; $l < 2; $l++) {
             // j'utilise mon provider pour récupérer un user unique
             $dataUser = $faker->unique()->user();
             // J'instancie un nouvel obJet user
-            $admin = new User();
+            $user = new User();
             // Je set l'email, c'est une string simple
-            $admin->setEmail($dataUser["email"]);
+            $user->setEmail($dataUser["email"]);
             // Je set les roles, c'est un tableau
-            $admin->setRoles($dataUser["roles"]);
-            $admin->setUsername($faker->text(15));
-            $admin->setFirstname($faker->firstName);
-            $admin->setLastname($faker->lastName);
+            $user->setRoles($dataUser["roles"]);
+            $user->setUsername($faker->text(15));
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
             // Je set le password, c'est une string simple
             // J'utilise le passwordHasher passé au constructeur, ça s'appelle de l'injection de dépendance (on en parlera dans les prochains jours), il contient la méthode hashPassword qui permet de changer une string en hash par rapport à la méthode de hashage définis dans security.yaml
             // ! SI PAS DE HASH, PAS POSSIBLE DE S'AUTHENTIFIER
             // ! DE PLUS UN MDP EN CLAIR EN BDD EST A LA LIMITE DE L'ILLEGALITE
-            $admin->setPassword($this->passwordHasher->hashPassword($admin, $dataUser["password"]));
+            $user->setPassword($this->passwordHasher->hashPassword($user, $dataUser["password"]));
         
             // On oublis pas de persister l'objet
-            $manager->persist($admin);
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        //! UserCollection avec les utilisateurs créés en dur
+
+        $userCollectionUsers = [];
+        for ($l = 0; $l < 2; $l++) {
+            $userCollection = new UserCollection();
+            $userCollection->setStatus($faker->randomElement([0, 1, 2]));
+        
+            // Link the user and the user collection (randomly choose from existing users)
+            $randomUser = $faker->randomElement($users);
+            if (!in_array($randomUser, $userCollectionUsers)) {
+                $userCollection->setUser($randomUser);
+                $userCollectionUsers[] = $randomUser;
+            }
+        
+            // Link the comics and the user collection (your existing code)
+        
+            $manager->persist($userCollection);
         }
 
         $manager->flush();
