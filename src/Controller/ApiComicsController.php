ac1e31cb473a034430783a23f1886e8308ffea25
NicolasDeniau <nicolas.deniau@oclock.school>
2023-08-29 09:48:22 +0200

    feat-custom-query-search-comics-by-title

--- src/Controller/ApiComicsController.php
+++ src/Controller/ApiComicsController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Comics;
 use App\Repository\ComicsRepository;
 use App\Repository\CharactersRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ApiComicsController extends AbstractController
@@ -32,5 +33,31 @@ class ApiComicsController extends AbstractController
         $json = $serializer->serialize($comics, 'json', ['groups' => 'comics']);
 
         return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/api/search-comics", name="api_search_comics")
+     */
+    public function searchComics(Request $request, ComicsRepository $comicsRepository): JsonResponse
+    {
+         // Retrieve the title of the comics
+        $title = $request->query->get('title');
+
+        // Use the custom query in order to do the search by title
+        $comics = $comicsRepository->findAllOrderByTitleSearch($title);
+
+        // Convert the array as a json
+        $response = [];
+        foreach ($comics as $comic) {
+            $response[] = [
+                'id' => $comic->getId(),
+                'title' => $comic->getTitle(),
+                'poster' => $comic->getPoster(),
+                'synopsis' => $comic->getSynopsis(),
+                'released_at' => $comic->getReleasedAt(),
+            ];
+        }
+
+        return $this->json($response, Response::HTTP_OK, [], ['groups' => 'comics']);
     }
 }
