72f3d60d7b07aa284c544cde36326f4eeb8db4bc
NicolasDeniau <nicolas.deniau@oclock.school>
2023-09-04 09:55:55 +0200

    build/add-KernelException

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    //This method allow us to get an error as a JSON when we created a comics with a JSON that is not valid, rather than a symfony error
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage()
+            ];
+
+            $event->setResponse(new JsonResponse($data));
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => $exception->getMessage()
+            ];
+
+            $event->setResponse(new JsonResponse($data));
+        }
+    }
+
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
