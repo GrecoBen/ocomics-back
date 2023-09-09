d5b3a45adf52dc81e19d77254d8b2d4bd79d3a78
GrecoBen <greco.benjamin@orange.fr>
2023-09-09 21:48:46 +0200

    fixed-routing

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+/**
+     * @Route("/{reactRouting}", name="react_routing", defaults={"reactRouting": null}, requirements={"reactRouting"=".+"})
+     */
+    public function index(): Response
+    {
+        // Remplacez cette partie par le chemin vers votre fichier index.html
+        return $this->file('%kernel.project_dir%/public/index.html');
+    }
+}
